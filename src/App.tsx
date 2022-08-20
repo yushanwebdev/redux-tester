d6fbd399b6d4448684f21f748864140b75517265
yushan <yushanwebdev@gmail.com>
2022-08-20 11:44:16 +0530

    feat: Develop the Test component - Develop it to test the re-renders on the Redux State Change - Configure the default formatter in the Workspace to the Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Test from "./app/Test";
 import Counter from "./features/counter/Counter";
 
 function App(): JSX.Element {
   return (
     <div className="App">
+      <Test />
       <Counter />
     </div>
   );
---
+++ src/app/Test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Test = () => {
+  console.count("count");
+
+  return (
+    <div>
+      <p className="text-2xl">I&apos;m using Redux</p>
+    </div>
+  );
+};
+
+export default Test;
